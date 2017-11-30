595416c5eaa5e602bd2d2732ea64cddf28b70c76
John Alex Ladra <johnalexladra@gmail.com>
2017-12-01 07:37:43 +0800

    Add new files

---
+++ main.js
@@ -0,0 +1,47 @@
+angular.module('todoApp', [])
+    .controller('TodoListController', function() {
+        var todoList = this;
+        todoList.todos = [{
+            text: 'Drink some coffee',
+            done: true
+        }, {
+            text: 'Learn AngularJS',
+            done: true
+        }, {
+            text: 'Build an AngularJS app',
+            done: true
+        }, {
+            text: 'Grow a beard',
+            done: false
+        }, {
+            text: 'Rule the world!',
+            done: false
+        }];
+
+        todoList.addTodo = function() {
+            todoList.todos.push({
+                text: todoList.todoText,
+                done: false
+            });
+            todoList.todoText = '';
+        };
+
+        todoList.remaining = function() {
+            var count = 0;
+            angular.forEach(todoList.todos, function(todo) {
+                count += todo.done ? 0 : 1;
+            });
+            return count;
+        };
+
+        todoList.archive = function() {
+            var oldTodos = todoList.todos;
+            todoList.todos = [];
+            angular.forEach(oldTodos, function(todo) {
+                if (!todo.done) todoList.todos.push(todo);
+            });
+        };
+    });
+
+
+
